0052fc3e332933fadb6b9d7510b7aca90b1ce8dc
rodrigo <rodrigosv91@gmail.com>
2024-01-09 22:54:50 -0300

    add: toggleTask

--- src/App.vue
+++ src/App.vue
@@ -65,9 +65,22 @@ export default {
         ? (this.tasks = this.tasks.filter((task) => task.id !== id))
         : alert("Error Deleting");
     },
-    toggleReminder(id: number) {
+    async toggleReminder(id: number) {
+      const taskToToggle = await this.fetchTaskById(id);
+      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updTask),
+      });
+
+      const data = await res.json();
+
       this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       );
     },
     async fetchTask() {
